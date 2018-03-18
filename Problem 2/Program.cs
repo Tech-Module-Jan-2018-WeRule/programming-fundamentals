996aabd6eae3808fbb72f4d6cff96e38bd74a090
stanvchev <stanvchev@gmail.com>
2018-03-18 17:26:18 +0200

    Added comments to Problem 2/Program.cs

--- Problem 2/Program.cs
+++ Problem 2/Program.cs
@@ -11,11 +11,13 @@ namespace figureOfStars
     {
         static void Main(string[] args)
         {
+            //in the next 4 lines we are getting the input for our program - the two sides of the figure of stars that we are going to print
             Console.Write("Enter Short side of figure: ");
             var Short_size = int.Parse(Console.ReadLine());
             Console.Write("Enter Long side of figure: ");
             var Long_size = int.Parse(Console.ReadLine());
 
+            //this for cycle prints the figure according to the input
             for (int rows = 0; rows != Short_size; rows++)
              {
                 for (int cols= 0; cols != Long_size; cols++)
