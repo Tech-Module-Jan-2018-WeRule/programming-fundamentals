d516eb36fea752cd7db24cff31b18e11292e0c06
EvgeniyaDelcheva <evgeniya.delcheva@gmail.com>
2018-03-18 17:10:11 +0200

    add problem 2 and problem 4

---
+++ Problem 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace figureOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Short side of figure: ");
+            var Short_size = int.Parse(Console.ReadLine());
+            Console.Write("Enter Long side of figure: ");
+            var Long_size = int.Parse(Console.ReadLine());
+
+            for (int rows = 0; rows != Short_size; rows++)
+             {
+                for (int cols= 0; cols != Long_size; cols++)
+                {
+                    if (rows == 0 || rows == Short_size - 1 || cols == 0 || cols == Long_size - 1) Console.Write("* ");
+                    else Console.Write("  ");
+                }
+                Console.WriteLine(" ");
+            }
+            
+
+
+        
+
+        }
+    }
+}
