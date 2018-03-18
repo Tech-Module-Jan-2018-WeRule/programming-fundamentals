2fe4dd0f7adb699108e585ce0e5fc2a0097cafb0
Miki761000 <miki761000@gmail.com>
2018-03-18 16:50:42 +0200

    Add Problem 3

---
+++ Problem 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace C_Problem_1._Choose_a_Drink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string profecion = Console.ReadLine();
+
+            switch (profecion)
+            {
+                case "Athlete":
+                    Console.WriteLine("Water");
+                    break;
+                case "Businessman":
+                case "Businesswoman":
+                    Console.WriteLine("Coffee");
+                    break;
+                case "SoftUni Student":
+                    Console.WriteLine("Beer");
+                    break;
+               
+                default:
+                    Console.WriteLine("Tea");
+                    break;
+            }
+
+
+           
+        }
+    }
+}
