717c2b21da01a5ebd846cc7dda018fc04b9e27db
Viliqn <viliqnvachev@abv.bg>
2018-03-19 14:37:00 +0200

    added comments to the code of Solution of Problem 1;

--- Problem 1/Phonebook/Program.cs
+++ Problem 1/Phonebook/Program.cs
@@ -9,18 +9,26 @@
         public static void Main(string[] args)
         {
 
-// change name of token to token
+            // change name of token to token
+            // We recieve from the console some info from the console about people and their phone numbers.
 
-            var token= Console.ReadLine().Split().ToList();
+            var token = Console.ReadLine().Split().ToList();
 
             var phonebook = new Dictionary<string, string>();
 
+            // If we recieve "END"– stop receiving more commands.
+
             while (token[0] != "END")
             {
+                // Adds entry to the phonebook. In case of trying to add a name that is already in the phonebook we 
+                // should change the existing phone number with the new one provided.
                 if (token[0] == "A" && token.Count > 2)
                 {
                     phonebook[token[1]] = token[2];
                 }
+
+                // Searches for a contact by given name and prints it in format "{name} -> {number}".
+                // In case the contact isn't found, print "Contact {name} does not exist.".
                 else
                 {
                     if (phonebook.ContainsKey(token[1]))
@@ -32,7 +40,7 @@
                         Console.WriteLine("Contact {0} does not exist.", token[1]);
                     }
                 }
-                token= Console.ReadLine().Split().ToList();
+                token = Console.ReadLine().Split().ToList();
             }
         }
     }
