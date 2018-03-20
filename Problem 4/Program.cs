918118c17defe442208729f805fc9bbdeb648231
Viliqn <viliqnvachev@abv.bg>
2018-03-20 15:46:42 +0200

    checked the problem 4

--- Problem 4/Program.cs
+++ Problem 4/Program.cs
@@ -10,6 +10,7 @@ namespace Nums1To20
     {
         static void Main(string[] args)
         {
+            //CONFIRMED  from <viliqn>
             //Writing numbers from 1 to 20 on a separate lines.
             //no variable to be changed, so I will change this to a for cycle at least
             for (int i = 1; i < 21; i++)
